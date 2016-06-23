3dd22dd15861ef334fce5c21430fb6a8a782dd74
Shipra Gupta <shipra2483@users.noreply.github.com>
2016-06-23 18:00:00 +0530

    Add files via upload

---
+++ matrix_data_frame_shipra.R
@@ -0,0 +1,27 @@
+swirl()
+my_vector <- 1:20
+my_vector
+dim(my_vector)
+length(my_vector)
+length(my_vector) <- c(4, 5)
+dim(my_vector) <- c(4, 5)
+dim (my_vector)
+attributes(my_vector)
+my_vector
+class(my_vector)
+my_matrix <- my_vector
+matrix()
+?matrix
+my_matrix2 <- matrix(data=(1:20), nrow = 4, ncol = 5)
+identical(my_matrix, my_matrix2)
+patients <- c("Bill", "Gina", "Kelly", "Sena")
+patients <- c("Bill", "Gina", "Kelly", "Sean")
+cbind(patients, my_matrix)
+my_data <- data.frame(patients, my_matrix)
+my_data
+class(my_data)
+cname <- c("patient", "age", "weight", "bp", "rating", "test")
+cnames <- c("patient", "age", "weight", "bp", "rating", "test")
+colnames(my_data) <- cnames
+my_data
+savehistory("~/DATA_SCIENCE/matrix_data_frame_shipra.R")
