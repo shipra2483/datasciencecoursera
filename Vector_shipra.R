b0b305edd8f49a5f8f53b06388e1c9e57601000c
Shipra Gupta <shipra2483@users.noreply.github.com>
2016-06-23 18:01:22 +0530

    Add files via upload

---
+++ Vector_shipra.R
@@ -0,0 +1,18 @@
+swirl()
+num_vect <- c(0.5, 55, -10, 6)
+tf <- num_vect < 1
+tf
+num_vect >= 6
+my_char <- c("My", "name", "is")
+my_char
+paste(my_char, collapse = " ")
+my_name <- c(my_char, "Shipra")
+my_name
+paste(my_name, collapse = " " )
+paste("HEllo", "World!", sep = " " )
+paste("Hello", "world!", sep = " ")
+paste(1:3, c("X", "Y", "Z"), sep = " ")
+paste(1:3, c("X", "Y", "Z"), sep = "")
+paste(LETTERS, 1:4, sep = "-")
+savehistory("~/DATA_SCIENCE/Vector.R")
+savehistory("~/DATA_SCIENCE/Vector.R")
