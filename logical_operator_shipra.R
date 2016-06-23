ef7d7cd4dad96de098726a736c834b01c434dd40
Shipra Gupta <shipra2483@users.noreply.github.com>
2016-06-23 17:59:44 +0530

    logic

---
+++ logical_operator_shipra.R
@@ -0,0 +1,28 @@
+swirl()
+TRUE == TRUE
+(FALSE == TRUE) == FALSE
+6 == 7
+6 < 7
+1- <= 10
+10 <= 10
+!5 == 7
+5! == 7
+5 != 7
+!5 == 7
+FALSE & FASE
+FALSE & FALSE
+TRUE & c(TRUE, FALSE, FALSE)
+TRUE && c(TRUE, FALSE, FALSE)
+TRUE | c(TRUE, FALSE, FALSE)
+TRUE || c(TRUE, FALSE, FALSE)
+5 > 8 || 6 != 8 && 4 > 3.9
+isTRUE(6 > 4)
+identical('twins', 'twins')
+xor(5 == 6, !FALSE)
+ints <- sample(10)
+ints
+ints > 5
+which(ints > 7)
+any(ints < 0)
+all(ints > 0)
+savehistory("~/DATA_SCIENCE/logical_operator_shipra.R")
