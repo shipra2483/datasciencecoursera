acbcf6d7d7127704b434fb4bdae68777cf17f8f6
Shipra Gupta <shipra2483@users.noreply.github.com>
2016-06-23 18:00:15 +0530

    Add files via upload

---
+++ missing_values_shipra.R
@@ -0,0 +1,16 @@
+swirl()
+x <- c(44, NA, 5, NA)
+x * 3
+y <- rnorm(1000)
+z <- rep(NA, 1000)
+my_data <- sample(c(y, z), 100)
+my_na <- is.na(my_data)
+my_na
+my_data == Na
+my_data == NA
+sum(my_na)
+my_data
+0/0
+Inf - Inf
+savehistory("~/Missing_values.R")
+savehistory("~/DATA_SCIENCE/missing_values.R")
