dc7d1d6a0d320b976bf72515a268bf1ef667d368
Shipra Gupta <shipra2483@users.noreply.github.com>
2016-06-28 15:04:50 +0530

    Add files via upload

---
+++ lapply_sapply_shipra.R
@@ -0,0 +1,29 @@
+library(swirl)
+swirl()
+head(flags)
+dim(flags)
+class(flags)
+cls_list <- lapply(flags, class)
+cls_list
+class(cls_list)
+as.character(cls_list)
+cls_vect <- sapply(flags, class)
+class(cls_vect)
+sum(flags$orange)
+flag_colors <- flags[, 11:17]
+head(flag_colors)
+lapply(flag_colors, sum)
+sapply(flag_colors, sum)
+sapply(flag_colors, mean)
+flag_shapes <- flags[, 19:23]
+lapply(flag_shapes, range)
+shape_mat <- sapply(flag_shapes, range)
+shape_mat
+class(shape_mat)
+unique(c(3, 4, 5, 5, 5, 6, 6))
+unique_vals <- lapply(flags, unique)
+unique_vals
+sapply(unique_vals, length)
+sapply(flags, unique)
+lapply(unique_vals, function(elem)elem[2])
+savehistory("C:/Users/shipra.gupta/Desktop/datasciencecoursera/lapply_sapply_shipra.R")
